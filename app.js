9d09e74ad2fe73482a1f2d79a1ca291d3182f914
John Wiebe <johnjw59@gmail.com>
2026-08-15 23:59:15 -0700

    Include date in any console calls

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const { WebhookClient } = require('discord.js');
 require('dotenv').config();
 const webhookClient = new WebhookClient({ id: process.env.DISCORD_WEBHOOK_ID, token: process.env.DISCORD_WEBHOOK_TOKEN });
 
+// Include date in any console.log() calls.
+const origLog = console.log;
+console.log = (...args) => {
+  const ts = new Date().toISOString();
+  origLog(`[${ts}]`, ...args);
+};
+
 // Listen for alerts and send the included messages to Discord.
 alertEvents.onAlert((message) => {
   webhookClient.send({
